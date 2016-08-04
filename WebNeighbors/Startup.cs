8604dc425b9d4bc04aec59d09d605c3c8a8ca47d
Lyudmila Romanovskaya <lRomanovskaya@ntoire-polus.ru>
2016-08-04 14:47:45 +0300

    Add project files.

---
+++ WebNeighbors/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebNeighbors.Startup))]
+namespace WebNeighbors
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
