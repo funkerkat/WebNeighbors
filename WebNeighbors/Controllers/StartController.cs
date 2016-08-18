ea6cc31f0bd9a8049a42616b4bf1a9c64297414c
Lyudmila Romanovskaya <lRomanovskaya@ntoire-polus.ru>
2016-08-18 09:16:59 +0300

    Add database and input form to main view.

--- WebNeighbors/Controllers/StartController.cs
+++ WebNeighbors/Controllers/StartController.cs
@@ -1,17 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using WebNeighbors.Models;
 
 namespace WebNeighbors.Controllers
 {
   public class StartController : Controller
   {
+    PersonContext db = new PersonContext();
+
     // GET: Start
     public ActionResult Index()
     {
-      return View();
+      return View(db.Persons);
+    }
+
+    [HttpPost]
+    public ActionResult Index(Person person)
+    {
+      db.Persons.Add(person);
+      db.SaveChanges();
+      return View(db.Persons);
     }
 
     public ActionResult Temp()
