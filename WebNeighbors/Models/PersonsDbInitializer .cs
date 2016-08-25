83bf7e3471086da1f68ca25f39a9fc1a70daaf42
Lyudmila Romanovskaya <lromanovskaya@ntoire-polus.ru>
2016-08-25 10:42:05 +0300

    Replace baloons outputs to loop.

--- WebNeighbors/Models/PersonsDbInitializer .cs
+++ WebNeighbors/Models/PersonsDbInitializer .cs
@@ -7,8 +7,8 @@ namespace WebNeighbors.Models
   {
     protected override void Seed(PersonContext db)
     {
-      db.Persons.Add(new Person {PersonName = "Василий", ServiceName = "дворник", ServiceDescription = "Подметаю во дворе.", CoordinateX = 55.86, CoordinateY = 37.74 });
-      db.Persons.Add(new Person { PersonName = "Марья", ServiceName = "уборщица", ServiceDescription = "Мою окна.", CoordinateX = 55.88, CoordinateY = 37.74 });
+      db.Persons.Add(new Person {PersonName = "Василий", ServiceName = "дворник", ServiceDescription = "Подметаю во дворе.", CoordinateX = 55.871030, CoordinateY = 37.658510 });
+      db.Persons.Add(new Person { PersonName = "Марья", ServiceName = "уборщица", ServiceDescription = "Мою окна.", CoordinateX = 55.782392, CoordinateY = 37.614924 });
 
       base.Seed(db);
     }
